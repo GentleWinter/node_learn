f13c79cccac673127d7959fd165577ac4b400125
Winter <36679448+GentleWinter@users.noreply.github.com>
2022-02-04 20:55:52 -0300

    criando POST

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,3 +1,8 @@
+const res = require("express/lib/response");
+
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('voce esta na rota de atendimentos'));
+
+    app.get('/atendimentos', (req, res) => res.send('voce esta na rota de atendimentos e realizando um GET'));
+
+    app.post('/atendimentos', (req, res) => res.send(`voce esta na rota de atendimentos e realizando um POST`));
 }
