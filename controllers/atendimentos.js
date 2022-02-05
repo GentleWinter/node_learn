107421bb3d65492f3520fff87c55b8f15c5052f2
Winter <36679448+GentleWinter@users.noreply.github.com>
2022-02-04 21:23:26 -0300

    recebendo agendamentos

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -4,5 +4,8 @@ module.exports = app => {
 
     app.get('/atendimentos', (req, res) => res.send('voce esta na rota de atendimentos e realizando um GET'));
 
-    app.post('/atendimentos', (req, res) => res.send(`voce esta na rota de atendimentos e realizando um POST`));
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body);
+        res.send(`voce esta na rota de atendimentos e realizando um POST`)
+    });
 }
