bbf74165e9988011858a910f4c921e0423d8e3e5
Winter <36679448+GentleWinter@users.noreply.github.com>
2022-02-06 19:17:01 -0300

    conexao e tabelas

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const customExpress = require('./config/customExpress.js');
+const conexao = require('./infra/conexao.js');
+const Tabelas = require('./infra/tabelas.js');
 
-const app = customExpress();
 
-app.listen(3000, () => console.log('listen to port 3000'));
+conexao.connect(erro => {
+    if(erro){
+        console.log('Deu ruim: ',erro);
+    }
+    console.log('Devidamente conectado');
+    Tabelas.init(conexao);
+    const app = customExpress();
+    app.listen(3000, () => console.log('listen to port 3000'));
+})
