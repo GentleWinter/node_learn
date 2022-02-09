fb1e152130d66d38193274c8011ab2da5b611fed
Winter <36679448+GentleWinter@users.noreply.github.com>
2022-02-08 21:01:41 -0300

    ainda pendente formatacao data

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,10 +1,14 @@
+const moment = require(`moment`);
 const { conexao } = require("../infra/tabelas");
 
 class Atendimentos {
     adiciona(atendimentos){
+        const dataCriacao = moment().format(`YYYY-MM-DD HH:MM:ss`);
+        const data = moment(atendimentos.data, `DD/MM/YYY`).format(`YYYY-MM-DD HH:MM:ss`);
+        const atendimentoDatado = {...atendimentos, dataCriacao, data}
         const sql = `INSERT INTO atendimentos SET ?`;
 
-        conexao.query(sql, atendimentos, (erro, resultados) => {
+        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
             if(erro){
                 console.log(erro);
             }
