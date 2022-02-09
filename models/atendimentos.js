d03edf71069886b92324a27db0ac210f98b60970
Winter <36679448+GentleWinter@users.noreply.github.com>
2022-02-08 21:20:27 -0300

    Correção falha data agora devidamente implementado

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -4,7 +4,7 @@ const { conexao } = require("../infra/tabelas");
 class Atendimentos {
     adiciona(atendimentos){
         const dataCriacao = moment().format(`YYYY-MM-DD HH:MM:ss`);
-        const data = moment(atendimentos.data, `DD/MM/YYY`).format(`YYYY-MM-DD HH:MM:ss`);
+        const data = moment(atendimentos.data, `DD/MM/YYYY HH:MM:ss`).format(`YYYY-MM-DD HH:MM:ss`);
         const atendimentoDatado = {...atendimentos, dataCriacao, data}
         const sql = `INSERT INTO atendimentos SET ?`;
 
