9fe4267e7832deb01d6f031698a413f66bc9fcc6
Winter <36679448+GentleWinter@users.noreply.github.com>
2022-02-10 22:03:51 -0300

    validate data

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -2,21 +2,47 @@ const moment = require(`moment`);
 const { conexao } = require("../infra/tabelas");
 
 class Atendimentos {
-    adiciona(atendimentos){
+    adiciona(atendimentos, res){
         const dataCriacao = moment().format(`YYYY-MM-DD HH:MM:ss`);
         const data = moment(atendimentos.data, `DD/MM/YYYY HH:MM:ss`).format(`YYYY-MM-DD HH:MM:ss`);
-        const atendimentoDatado = {...atendimentos, dataCriacao, data}
-        const sql = `INSERT INTO atendimentos SET ?`;
+        
+        const dataEhValida = moment(data).isSameOrAfter(dataCriacao);
+        const clienteEhValido = atendimentos.cliente.length >= 5;
 
-        conexao.query(sql, atendimentoDatado, (erro, resultados) => {
-            if(erro){
-                console.log(erro);
+        const validacoes = [
+            {
+                nome: `data`,
+                valido: dataEhValida,
+                mensagem: `Data deve ser igual ou posterior à atual`
+            },
+            {
+                nome: `cliente`,
+                valido: clienteEhValido,
+                mensagem: `Nome de cliente deve conter mais de 5 caracteres`
             }
-            else{
-                console.log(resultados);
-            }
+        ]
 
-        });
+        const erros = validacoes.filter(campo => !campo.valido);
+        const existemErros = erros.length;
+
+        if(existemErros){
+            res.status(400).json(erros);
+        }
+        else{
+            const atendimentoDatado = {...atendimentos, dataCriacao, data}
+    
+            const sql = `INSERT INTO atendimentos SET ?`;
+        
+            conexao.query(sql, atendimentoDatado, (erro, resultados) => {
+                if(erro){
+                    res.status(400).json(erro);
+                }
+                else{
+                    res.status(201).json(resultados);
+                }
+
+            });
+        }
     }
 }
 
